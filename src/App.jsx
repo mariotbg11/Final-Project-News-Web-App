93c9d7576b712441197af364c7c53fa044a0e3df
mariotbg11 <christofell.m@gmail.com>
2024-09-23 13:51:46 +0700

    feat: adding header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <Header />;
+    </>
+  );
 }
 
 export default App;
