fe91740c9dc40eaa907407ad81fb791804fe98f6
mariotbg11 <christofell.m@gmail.com>
2024-09-23 15:13:16 +0700

    feat: fetching-news-api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const [indonesiaNews, setIndonesiaNews] = useState([]);
+  const [programmingNews, setProgrammingNews] = useState([]);
+
+  useEffect(() => {
+    const apiKey = import.meta.env.VITE_NEWS_API_KEY;
+    const url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=Indonesia&fq=headline:("Indonesia") AND document_type:("article")&api-key=${apiKey}`;
+
+    async function getIndonesiaNews() {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+      setIndonesiaNews(data);
+    }
+    getIndonesiaNews();
+  }, []);
+
+  useEffect(() => {
+    const apiKey = import.meta.env.VITE_NEWS_API_KEY;
+    const url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=Computer&fq=section_name:("Technology")&api-key=${apiKey}`;
+
+    async function getProgrammingNews() {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+      setProgrammingNews(data);
+    }
+    getProgrammingNews();
+  }, []);
+
   return (
     <>
       <Navbar />
